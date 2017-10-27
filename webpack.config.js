9d4c8a7ae17757e58e6ef45f6ebb34170b90b538
Kim HyoSeon <khs7515@naver.com>
2017-10-27 23:06:24 +0900

    edit

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     devServer: {
         inline: true,
         port: 7777, 
-        contentBase: __dirname + '/dist/', // 실행 파일들이 위치한 path
+        contentBase: __dirname, // 실행 파일들이 위치한 path
         stats: 'errors-only' // 재생성 시 에러 메세지만 보기
     },
 
@@ -41,13 +41,14 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: 'src/index.html', // 가져올 템플릿 경로
-            //filename: '../index.html', // 최종 생성 경로
+            filename: '../index.html', // 최종 생성 경로
             /*minify: {
                 collapseWhitespace: true
             }, // minify html*/
             hash: true // auto increase script version 
         }),
         new ExtractTextPlugin({
+
             filename: "app.css"            
         }),
     ]
