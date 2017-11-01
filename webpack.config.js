6f39ff2ef56af3ee14333f128ab5e53e59fb37f2
kimhyoseon <khs7515@naver>
2017-11-01 16:50:33 +0900

    edit

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,76 @@
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-    entry: './src/index.js',
+module.exports = env => {
+    
+    /* variables */
+    const ENV = env || {}
+    const isProd = ENV.production;    
+    const htmlPath = isProd ? __dirname + "/" : __dirname + "/dist/";    
+    const cssUse = {
+        dev: ['style-loader', 'css-loader'],
+        prod: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: "css-loader"        
+        })
+    }                
 
-    output: {
-        path: __dirname + '/dist/',
-        filename: 'bundle.js'
-    },
+    /* config */
+    return {
+        entry: {
+            app: __dirname + '/src/app.js'
+        },    
 
-    devServer: {
-        inline: true,
-        port: 7777, 
-        contentBase: __dirname, // 실행 파일들이 위치한 path
-        stats: 'errors-only' // 재생성 시 에러 메세지만 보기
-    },
+        output: {
+            path: __dirname + '/dist/',
+            filename: 'bundle.js'
+        },
 
-    module: {
-        rules: [
-            {   
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env', 'react']
+        devServer: {
+            inline: true,
+            port: 7777, 
+            contentBase: __dirname + '/dist/', // 실행 파일들이 위치한 path
+            stats: 'errors-only', // 재생성 시 에러 메세지만 보기        
+            hot: true
+        },
+
+        module: {
+            rules: [
+                {   
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['env', 'react']
+                        }
                     }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader"
-                })
-            }               
-        ]        
-    },
+                },
+                {
+                    test: /\.css$/,
+                    use: isProd ? cssUse.prod : cssUse.dev
+                }               
+            ]        
+        },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html', // 가져올 템플릿 경로
-            filename: '../index.html', // 최종 생성 경로
-            /*minify: {
-                collapseWhitespace: true
-            }, // minify html*/
-            hash: true // auto increase script version 
-        }),
-        new ExtractTextPlugin({
-
-            filename: "app.css"            
-        }),
-    ]
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: __dirname + '/src/template/index.html', // 가져올 템플릿 경로
+                filename: htmlPath + "index.html", // 최종 생성 경로
+                minify: {
+                    collapseWhitespace: isProd
+                }, // minify html
+                hash: true // auto increase script version 
+            }),
+            new ExtractTextPlugin({
+                filename: "app.css",
+                allChunks: true,
+                publicPath: __dirname + "/dist/",
+                disable: !isProd 
+            }),        
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NamedModulesPlugin()
+        ]
+    }
 };
