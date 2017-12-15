20cccc2aedc9415eaa66f161a642b6c94e30b90d
kimhyoseon <khs7515@naver>
2017-12-15 13:51:17 +0900

    수정

--- src/app.js
+++ src/app.js
@@ -3,34 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const html = (
-  <div className="container">
-    
+  <div className="container">    
     <div className="starter-template">
-      <h1>Hello, world</h1>          
-
-      <button type="button" className="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-        modal button
-      </button>    
-
-      <div className="modal fade" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-              <h4 className="modal-title" id="myModalLabel">Modal title</h4>
-            </div>
-            <div className="modal-body">
-              Modal success~!
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary">Save changes</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <h1>뽐집사 Project</h1>          
     </div>
-
   </div>
 );
 
