7fbe2433e00e77f2d00be3265301f9781fcd8598
kimhyoseon <khs7515@naver>
2019-01-16 13:44:11 +0900

    commit

--- api/translate.php
+++ api/translate.php
@@ -8,7 +8,7 @@ try {
 
     define('DEBUG', filter_input(INPUT_GET, 'debug'));
 
-    // define('KEYWORD', '쇼킹팟');
+    // define('KEYWORD', '설화수에센셜립세럼4호');
 
     if (DEBUG == true) define('KEYWORD', (filter_input(INPUT_GET, 'keyword', FILTER_SANITIZE_STRING)));
     else define('KEYWORD', (filter_input(INPUT_POST, 'keyword', FILTER_SANITIZE_STRING)));
@@ -29,16 +29,20 @@ try {
         'text'=> KEYWORD,
     ));
 
-    if (!empty($result) && !empty($result['message']) && !empty($result['message']['result']['translatedText']) && KEYWORD != $result['message']['result']['translatedText']) {        
-        $result['message']['result']['translatedText'] = urlencode(iconv("UTF-8", "EUC-CN", $result['message']['result']['translatedText']));                
-    } else {        
+    if (!empty($result) && !empty($result['message']) && !empty($result['message']['result']['translatedText']) && KEYWORD != $result['message']['result']['translatedText']) {
+        $result['message']['result']['translatedText'] = @iconv("UTF-8", "EUC-CN", $result['message']['result']['translatedText']);
+    }
+
+    if (!empty($result['message']['result']['translatedText'])) {
+        $result['message']['result']['translatedText'] = urlencode($result['message']['result']['translatedText']);
+    } else {
         $result = $apiNaver->POST("https://openapi.naver.com/v1/language/translate", array(
             'source' => 'ko',
             'target' => 'en',
             'text'=> KEYWORD,
-        ));        
+        ));
     }
-    
+
     echo json_encode($result['message']['result']);
 } catch (Exception $e) {
     http_response_code($e->getCode());
