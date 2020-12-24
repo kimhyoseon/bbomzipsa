4bc75f7dcfdd3e94d0a76334304aeafcd921b226
kimhyoseon <khs7515@naver.com>
2020-12-24 16:38:41 +0900

    c

--- class/PHPExcelDownload.php
+++ class/PHPExcelDownload.php
@@ -478,6 +478,11 @@ class PHPExcelDownload {
                             $v = $this->getShortName($v);
                         }
 
+                        // 2020-12-24 우편번호 오류
+                        if ($filterIndexReverse[$k] == '우편번호') {
+                            continue;
+                        }
+
                         if ($filterIndexReverse[$k] == '옵션정보') {
                             if ($row[array_search('상품명', array_keys($filterMerged))] == '신선식품') {
                                 $_v = $this->getShortOptionJshk($v);
@@ -1370,7 +1375,13 @@ class PHPExcelDownload {
 
                     foreach ($value as $k => $v) {
                         if (empty($v)) continue;
+
                         if (in_array($k, $filterArray)) {
+                            // 2020-12-24 우편번호 오류
+                            if ($filterIndexReverse[$k] == '우편번호') {
+                                continue;
+                            }
+
                             $row[array_search($filterIndexReverse[$k], array_keys($filterMerged))] = $v;
                         }
                     }
